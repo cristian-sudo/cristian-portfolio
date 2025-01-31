6379010b22cf8a5c512208cea69db45369caebe2
brew-cristian <cplop@brewdigital.uk>
2025-01-31 14:39:37 +0000

    :art: Added the hero banner and the quote component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,55 @@
-"use client"
+"use client";
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Navbar: React.FC = () => {
     const [language, setLanguage] = useState('EN');
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [showNavbar, setShowNavbar] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [hasScrolled, setHasScrolled] = useState(false);
 
     const handleLanguageChange = (newLanguage: string) => {
         setLanguage(newLanguage);
         setDropdownOpen(false);
     };
 
+    const controlNavbar = () => {
+        if (typeof window !== 'undefined') {
+            if (window.scrollY > lastScrollY) {
+                setShowNavbar(false);
+            } else {
+                setShowNavbar(true);
+            }
+            setLastScrollY(window.scrollY);
+
+            if (window.scrollY > 10) {
+                setHasScrolled(true);
+            } else {
+                setHasScrolled(false);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', controlNavbar);
+
+            // Clean up the event listener on component unmount
+            return () => {
+                window.removeEventListener('scroll', controlNavbar);
+            };
+        }
+    }, [lastScrollY]);
+
     return (
-        <nav className="mx-auto py-9">
-            <div className="container mx-auto flex justify-between items-center">
+        <nav
+            className={`fixed py-2 top-0 left-0 right-0 transition-transform duration-300 ${
+                showNavbar ? 'transform translate-y-0' : 'transform -translate-y-full'
+            } ${hasScrolled ? 'bg-gray-950' : 'bg-transparent'} z-50`}
+        >
+            <div className="container mx-auto py-4 flex justify-between items-center">
                 <div className="text-lg font-bold">
                     <Link href="/">
                         <span className="text-accent">$</span>
@@ -22,10 +57,18 @@ const Navbar: React.FC = () => {
                     </Link>
                 </div>
                 <div className="space-x-6 flex items-center">
-                    <Link href="/" className="text-white"><span className='text-accent'>#</span>home</Link>
-                    <Link href="/projects" className="text-white"><span className='text-accent'>#</span>works</Link>
-                    <Link href="/contact" className="text-white"><span className='text-accent'>#</span>about-me</Link>
-                    <Link href="/contact" className="text-white"><span className='text-accent'>#</span>contacts</Link>
+                    <Link href="/" className="text-white">
+                        <span className="text-accent">#</span>home
+                    </Link>
+                    <Link href="/projects" className="text-white">
+                        <span className="text-accent">#</span>works
+                    </Link>
+                    <Link href="/contact" className="text-white">
+                        <span className="text-accent">#</span>about-me
+                    </Link>
+                    <Link href="/contact" className="text-white">
+                        <span className="text-accent">#</span>contacts
+                    </Link>
                     <div className="relative">
                         <button
                             onClick={() => setDropdownOpen(!dropdownOpen)}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import HeroBanner from "@/app/components/HeroBanner";
+import Quote from "@/app/components/Quote";
 
 export default function Home() {
   return (
-      <HeroBanner />
+      <>
+        <HeroBanner/>
+        <Quote/>
+          <HeroBanner/>
+      </>
   );
 }
