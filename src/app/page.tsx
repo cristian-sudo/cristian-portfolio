b9cb3f1fa368afa2252dc1130025f3dfa73a1aa5
cristian-sudo <cplop21@gmail.com>
2025-02-18 22:30:12 +0000

    :art: Fixed the build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+// app/page.tsx
 import React from 'react';
 import PageClient from "@/app/PageClient";
 import { ApiResponse } from "@/app/types";
+export const dynamic = 'force-dynamic';
 
 const HomePage = async () => {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/collections/pages/entries?filter[slug:is]=home`, {
             cache: 'no-store',
         });
-        const apiData: ApiResponse = await res.json();
 
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const apiData: ApiResponse = await res.json();
         const homePageData = apiData.data[0];
 
         if (!homePageData) {
